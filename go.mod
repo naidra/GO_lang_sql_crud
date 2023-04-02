2b0a34f1c7be04012ee6e7cdab0af84c3769c03a
Ardian Sallauka <ardi_c_cc@hotmail.com>
2023-04-02 11:23:46 +0200

    Database config data

---
+++ go.mod
@@ -0,0 +1,8 @@
+module GO_lang_sql_crud
+
+go 1.20
+
+require (
+	github.com/go-sql-driver/mysql v1.7.0
+	github.com/naidra/GO_lang_sql_crud v0.0.0-20230402075931-bcf10a813c24
+)
--- model/connect.go
+++ model/connect.go
@@ -4,12 +4,23 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+
+	"github.com/go-sql-driver/mysql"
 )
 
 var con *sql.DB
 
 func Connect() *sql.DB {
-	db, err := sql.Open("mysql", "root:1234@tcp(localhost:3306)/mysql")
+	// Capture connection properties.
+	cfg := mysql.Config{
+		User:   "sql7610344", // os.Getenv("DBUSER")
+		Passwd: "zhwh4d66Xe", // os.Getenv("DBPASS")
+		Net:    "tcp",
+		Addr:   "sql7.freesqldatabase.com:3306",
+		DBName: "sql7610344",
+	}
+	db, err := sql.Open("mysql", cfg.FormatDSN())
+	// db, err := sql.Open("mysql", "golang_user:golang_user@tcp(localhost:3306)/mysql")
 	if err != nil {
 		log.Fatal(err)
 	}
