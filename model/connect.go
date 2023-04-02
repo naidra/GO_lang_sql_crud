477d75ea3dab4b2d3211c5efe17bb68776146728
Ardian Sallauka <ardi_c_cc@hotmail.com>
2023-04-02 22:03:36 +0200

    Change in db connect file

--- model/connect.go
+++ model/connect.go
@@ -19,6 +19,16 @@ func Connect() *sql.DB {
 	// db, err := sql.Open("mysql", cfg.FormatDSN())
 	// db, err := sql.Open("mysql", "golang_user:golang_user@tcp(localhost:3306)/mysql")
 
+	// user := "sql7610344"
+	// pass := "zhwh4d66Xe"
+	// host := "sql7.freesqldatabase.com:3306"
+	// dbname := "sql7610344"
+	// connection := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8&parseTime=True&loc=Local", user, pass, host, dbname)
+	// db, err := sql.Open("mysql", connection)
+	// if err != nil {
+	// 	panic(err)
+	// }
+
 	db, err := sql.Open("mysql", "sql7610344:zhwh4d66Xe@tcp(sql7.freesqldatabase.com:3306)/sql7610344")
 	if err != nil {
 		log.Fatal(err)
