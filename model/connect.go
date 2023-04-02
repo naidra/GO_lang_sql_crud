e86e855ec3679127f480376bd2ce555e67b33373
Ardian Sallauka <ardi_c_cc@hotmail.com>
2023-04-02 15:03:27 +0200

    Added vercel config

--- model/connect.go
+++ model/connect.go
@@ -4,23 +4,21 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
-
-	"github.com/go-sql-driver/mysql"
 )
 
 var con *sql.DB
 
 func Connect() *sql.DB {
-	// Capture connection properties.
-	cfg := mysql.Config{
-		User:   "sql7610344", // os.Getenv("DBUSER")
-		Passwd: "zhwh4d66Xe", // os.Getenv("DBPASS")
-		Net:    "tcp",
-		Addr:   "sql7.freesqldatabase.com:3306",
-		DBName: "sql7610344",
-	}
-	db, err := sql.Open("mysql", cfg.FormatDSN())
+	// cfg := mysql.Config{
+	// 	User:   "sql7610344", // os.Getenv("DBUSER")
+	// 	Passwd: "zhwh4d66Xe", // os.Getenv("DBPASS")
+	// 	Net:    "tcp",
+	// 	Addr:   "sql7.freesqldatabase.com:3306",
+	// 	DBName: "sql7610344",
+	// }
+	// db, err := sql.Open("mysql", cfg.FormatDSN())
 	// db, err := sql.Open("mysql", "golang_user:golang_user@tcp(localhost:3306)/mysql")
+	db, err := sql.Open("mysql", "sql7610344:zhwh4d66Xe@tcp(sql7.freesqldatabase.com:3306)/sql7610344")
 	if err != nil {
 		log.Fatal(err)
 	}
